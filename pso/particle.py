5009f10805a09c09b512da4843e54e65f4a70bce
samcogan21 <cogansamuel@campus.technion.ac.il>
2024-05-28 12:12:18 +0300

    Updated and debugged both update_best_position func and update_position function. Both passed all tests that I tried.

--- pso/particle.py
+++ pso/particle.py
@@ -1,5 +1,6 @@
 from numpy import hsplit
 from numpy.random import uniform
+import numpy as np
 
 class Particle:
     """
@@ -25,18 +26,38 @@ class Particle:
                 "best_fitness" : self.best_fitness}
     
     def update_velocity(self, inertia_weight, cognitive_coeff, social_coeff, global_best_position):
+        """
+        Func for updating velocity of particle
+        Args:
+            self, inertia_weight, cognitive_coeff, social_coeff, global_best_position
+            The velocity can vary depending on how the user would like to allocate coeffs.
+        """
         cognitive_component = cognitive_coeff * uniform(0, 1, self.position.shape) * (self.best_position - self.position)
         social_component = social_coeff * uniform(0, 1, self.position.shape) * (global_best_position - self.position)
         self.velocity = inertia_weight * self.velocity + cognitive_component + social_component
 
     def update_position(self):
-        self.position += self.velocity
+        """
+        Func for updating position of particle
+            Args:
+                Self.
+                The function itself updates by the particle's velocity.
+               """
+        self.position = np.add(self.position, self.velocity).astype(np.float64)
+        #store position array in type float64
 
     def evaluate_fitness(self, objective_function):
-        return objective_function(*hsplit(self.position, self.position.shape[0]))
+        return float(objective_function(*hsplit(self.position, self.position.shape[0])))
 
     def update_best_position(self, objective_function):
+        """
+        Func for updating best position of particle
+            Args:
+                self, objective function.
+                The function itself updates by the particle's evaluate_fitness function for a certain objective function.
+        """
         current_fitness = self.evaluate_fitness(objective_function)
         if current_fitness < self.best_fitness:
-            self.best_position = self.position
+            self.best_position = np.copy(self.position)
             self.best_fitness = current_fitness
+
--- pso/particle_test.py
+++ pso/particle_test.py
@@ -1,28 +1,51 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+from copy import deepcopy
 import unittest
 import numpy as np
 
 from particle import Particle
 
+
 class Testparticle(unittest.TestCase):
     """
-    
+
     """
 
     @classmethod
     def setUpClass(cls):
         """
-        
-        """
-        cls.particle_1 = Particle(position=np.array([0, 0 ,0]), velocity=np.array([0, 0, 1]))
 
-    def test_update_velocity(self):
-        self.particle_1.update_velocity(inertia_weight=1, cognitive_coeff=0, social_coeff=0, global_best_position=np.array([1, 1, 1]))
+        """
+        cls.particle_1 = Particle(position=np.array([0, 0, 0]), velocity=np.array([0, 0, 1]))
+        cls.particle_2 = Particle(position=np.array([1]), velocity=np.array([-1]))
+
+    def test_update_particle_velocity(self):
+        particle_1 = deepcopy(self.particle_1)
+        particle_1.update_velocity(inertia_weight=1,
+                                   cognitive_coeff=0,
+                                   social_coeff=0,
+                                   global_best_position=np.array([1, 1, 1])
+                                   )
         self.assertSequenceEqual(self.particle_1.velocity.tolist(), np.array([0, 0, 1]).tolist())
 
+    def test_update_particle_position(self):
+        particle_1 = deepcopy(self.particle_1)
+        particle_1.update_position()
+        self.assertSequenceEqual(self.particle_1.position.tolist(), np.array([0, 0, 0]).tolist())
 
-    def test_update_position(self):
-        self.particle_1.update_position()
-        self.assertSequenceEqual(self.particle_1.position.tolist(), np.array([0, 0, 1]).tolist())
+    def test_update_particle_best_position(self):
+        particle_1 = deepcopy(self.particle_1)
+        particle_1.position = np.array([0.25, 0, 0])
+        particle_1.update_best_position(objective_function=lambda x, y, z: x ** 2 - 2 * x - 1)
+        print("particle_1 best fitness before update", particle_1.best_fitness)
+        print("particle_1 fitness before position update:",particle_1.evaluate_fitness(lambda x, y, z: x ** 2 - 2 * x - 1))
+        particle_1.velocity = np.array([-0.25, 0, 0])
+        particle_1.update_position()
+        print("particle_1 position and velocity:", particle_1)
+        print("particle_1 fitness after position update:", particle_1.evaluate_fitness(lambda x, y, z: x ** 2 - 2 * x - 1))
+        print("particle_1 best fitness after update",particle_1.best_fitness)
+        particle_1.update_best_position(objective_function=lambda x, y, z: x ** 2 - 2*x - 1)
+        self.assertSequenceEqual(particle_1.best_position.tolist(), np.array([0.25, 0, 0]).tolist())
+
