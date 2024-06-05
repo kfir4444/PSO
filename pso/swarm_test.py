7db3b47906d0180189740e2854606bcdb02a7b94
samcogan21 <cogansamuel@campus.technion.ac.il>
2024-06-05 12:12:05 +0300

    Created new test for the function update_global_best_position

--- pso/swarm_test.py
+++ pso/swarm_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-
+import copy
 from copy import deepcopy
 import unittest
 import numpy as np
@@ -19,14 +19,27 @@ class TestSwarm(unittest.TestCase):
         """
 
         """
-        cls.swarm1 = Swarm(2, 3,
+        cls.swarm1 = Swarm(5, 3,
                            {"low" : [-1, -1, -1],
                             "high" : [1, 1, 1]},
                            {"inertia_weight" : 0.5,
                             "cognitive_coeff" : 1,
                             "social_coeff" : 1,},
                             False)
-
+        cls.swarm2 = Swarm(5, 3,
+                           {"low": [-100, -100, -100],
+                            "high": [100, 100, 100]},
+                           {"inertia_weight": 0.5,
+                            "cognitive_coeff": 1,
+                            "social_coeff": 1, },
+                           False)
+        cls.swarm3 = Swarm(3, 1,
+                           {"low": [-1],
+                            "high": [1]},
+                           {"inertia_weight": 1,
+                            "cognitive_coeff": 1,
+                            "social_coeff": 1, },
+                           False)
     def test_initialize_particles(self):
         self.assertEqual(self.swarm1.num_particles, len(self.swarm1.particles))
 
@@ -34,4 +47,51 @@ class TestSwarm(unittest.TestCase):
             for coord in particle.position:
                 self.assertLessEqual(coord, 1)
                 self.assertLessEqual(-1, coord)
-    
+
+    def test_initialize_particles2(self):
+        self.assertEqual(self.swarm2.num_particles, len(self.swarm2.particles))
+
+        for particle in self.swarm2.particles:
+            for coord in particle.position:
+                self.assertLessEqual(coord, 100)
+                self.assertLessEqual(-100, coord)
+
+    def test_generate_random_position(self):
+        for par in self.swarm2.particles:
+            for coordinate in par.position:
+                self.assertLessEqual(coordinate, 100)
+                self.assertLessEqual(-100, coordinate)
+
+    def test_generate_random_velocity(self):
+        for par in self.swarm1.particles:
+            for coordinate in par.velocity:
+                self.assertLessEqual(coordinate, 2)
+                self.assertLessEqual(-2, coordinate)
+
+
+    def test_update_global_best_position(self):
+        particles = [
+                Particle(np.array([1, 2]), np.array([-1, -1])),
+                Particle(np.array([2, 3]), np.array([0, 0])),
+                Particle(np.array([4, 5]), np.array([1, 1]))
+            ]
+        swarm4 = Swarm(3, 2,
+                           {"low": [-5, -5],
+                            "high": [5, 5]},
+                           {"inertia_weight": 1,
+                            "cognitive_coeff": 0,
+                            "social_coeff": 0, },
+                           False)
+        swarm4.particles = particles
+
+        for p in swarm4.particles:
+            p.best_fitness = p.evaluate_fitness(objective_function=lambda x, y: x**2 +y**2)
+
+        # Update the global best position
+        swarm4.update_global_best_position()
+        self.assertEqual(swarm4.global_best_fitness, 5)
+        self.assertEqual(swarm4.global_best_position.tolist(), np.array([1, 2]).tolist())
+
+
+
+
